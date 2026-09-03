c7db9b61cff6fdc052098e7ceb5427e7d7df4c85
marsh6487 <marshcorey@gmail.com>
2026-09-03 09:10:33 -0500

    weather: add concurrent_weather_audio_test.c

---
+++ soh/tests/concurrent_weather_audio_test.c
@@ -0,0 +1,26 @@
+#include <assert.h>
+#include <stdint.h>
+
+#include "../src/code/concurrent_weather_audio.h"
+
+int main(void) {
+    ConcurrentWeatherAudioState state = { 0 };
+
+    assert(ConcurrentWeatherAudio_ShouldPlayRainSfx(&state));
+
+    ConcurrentWeatherAudio_TrackNatureChannel(&state, 0x0E, 1, 1, 0x0E, 1);
+    assert(!ConcurrentWeatherAudio_ShouldPlayRainSfx(&state));
+
+    ConcurrentWeatherAudio_TrackNatureChannel(&state, 0x0F, 1, 1, 0x0E, 1);
+    assert(!ConcurrentWeatherAudio_ShouldPlayRainSfx(&state));
+
+    ConcurrentWeatherAudio_TrackNatureChannel(&state, 0x0E, 1, 0, 0x0E, 1);
+    assert(ConcurrentWeatherAudio_ShouldPlayRainSfx(&state));
+
+    ConcurrentWeatherAudio_TrackNatureChannel(&state, 0x0E, 4, 1, 0x0E, 1);
+    assert(ConcurrentWeatherAudio_ShouldPlayRainSfx(&state));
+
+    assert(!ConcurrentWeatherAudio_ShouldPlayThunder(0x0001, 0x0001));
+    assert(ConcurrentWeatherAudio_ShouldPlayThunder(0x0028, 0x0001));
+    return 0;
+}
