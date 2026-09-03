f5d2f14f8cf1f5333118f3ded8fc0a0164f59a4c
marsh6487 <marshcorey@gmail.com>
2026-09-03 09:57:11 -0500

    weather: harden active nature-channel guards (concurrent_weather_audio.h)

--- soh/src/code/concurrent_weather_audio.h
+++ soh/src/code/concurrent_weather_audio.h
@@ -5,11 +5,13 @@
 
 typedef struct {
     uint8_t natureRainEnabled;
+    uint8_t natureLightningEnabled;
 } ConcurrentWeatherAudioState;
 
 static inline void ConcurrentWeatherAudio_TrackNatureChannel(ConcurrentWeatherAudioState* state,
                                                               uint8_t channelRange, uint8_t port, uint8_t value,
-                                                              uint8_t rainChannel, uint8_t enablePort) {
+                                                              uint8_t rainChannel, uint8_t lightningChannel,
+                                                              uint8_t enablePort) {
     uint8_t firstChannel = channelRange >> 4;
     uint8_t lastChannel = channelRange & 0xF;
 
@@ -20,15 +22,22 @@ static inline void ConcurrentWeatherAudio_TrackNatureChannel(ConcurrentWeatherAu
     if ((port == enablePort) && (rainChannel >= firstChannel) && (rainChannel <= lastChannel)) {
         state->natureRainEnabled = value != 0;
     }
+
+    if ((port == enablePort) && (lightningChannel >= firstChannel) && (lightningChannel <= lastChannel)) {
+        state->natureLightningEnabled = value != 0;
+    }
 }
 
-static inline int ConcurrentWeatherAudio_ShouldPlayRainSfx(const ConcurrentWeatherAudioState* state) {
-    return !state->natureRainEnabled;
-}
-
-static inline int ConcurrentWeatherAudio_ShouldPlayThunder(uint16_t currentMainBgm,
+static inline int ConcurrentWeatherAudio_ShouldPlayRainSfx(const ConcurrentWeatherAudioState* state,
+                                                            uint16_t currentMainBgm,
                                                             uint16_t natureAmbienceSequence) {
-    return currentMainBgm != natureAmbienceSequence;
+    return !state->natureRainEnabled || (currentMainBgm != natureAmbienceSequence);
+}
+
+static inline int ConcurrentWeatherAudio_ShouldPlayThunderSfx(const ConcurrentWeatherAudioState* state,
+                                                               uint16_t currentMainBgm,
+                                                               uint16_t natureAmbienceSequence) {
+    return !state->natureLightningEnabled || (currentMainBgm != natureAmbienceSequence);
 }
 
 #endif
