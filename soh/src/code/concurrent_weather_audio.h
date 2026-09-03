a2c86aade1b837fb4fe3ed7c6bfe0e308e74d6f7
marsh6487 <marshcorey@gmail.com>
2026-09-03 14:02:56 -0500

    feat: add proximity weather thunder selector

--- soh/src/code/concurrent_weather_audio.h
+++ soh/src/code/concurrent_weather_audio.h
@@ -2,11 +2,45 @@
 #define CONCURRENT_WEATHER_AUDIO_H
 
 #include <stdint.h>
+#include "sfx.h"
 
 typedef struct {
     uint8_t natureRainEnabled;
     uint8_t natureLightningEnabled;
 } ConcurrentWeatherAudioState;
+
+typedef enum {
+    CONCURRENT_WEATHER_THUNDER_SFX_OFF = 0,
+    CONCURRENT_WEATHER_THUNDER_SFX_CINEMATIC_LIGHTNING,
+    CONCURRENT_WEATHER_THUNDER_SFX_GANONDORF_LIGHT_ARROW_HIT,
+    CONCURRENT_WEATHER_THUNDER_SFX_PHANTOM_GANON_LIGHTNING_ATTACK,
+    CONCURRENT_WEATHER_THUNDER_SFX_PHANTOM_GANON_LIGHTNING_HIT,
+    CONCURRENT_WEATHER_THUNDER_SFX_PHANTOM_GANON_GROUND_THUNDER,
+    CONCURRENT_WEATHER_THUNDER_SFX_GANONDORF_THUNDER_IMPACT,
+    CONCURRENT_WEATHER_THUNDER_SFX_BARINADE_LIGHTNING_ATTACK,
+} ConcurrentWeatherThunderSfxPreset;
+
+static inline uint16_t ConcurrentWeatherAudio_ResolveThunderSfx(int32_t preset) {
+    switch (preset) {
+        case CONCURRENT_WEATHER_THUNDER_SFX_CINEMATIC_LIGHTNING:
+            return NA_SE_EV_LIGHTNING;
+        case CONCURRENT_WEATHER_THUNDER_SFX_GANONDORF_LIGHT_ARROW_HIT:
+            return NA_SE_EN_GANON_DD_THUNDER;
+        case CONCURRENT_WEATHER_THUNDER_SFX_PHANTOM_GANON_LIGHTNING_ATTACK:
+            return NA_SE_EN_FANTOM_THUNDER;
+        case CONCURRENT_WEATHER_THUNDER_SFX_PHANTOM_GANON_LIGHTNING_HIT:
+            return NA_SE_EN_FANTOM_HIT_THUNDER;
+        case CONCURRENT_WEATHER_THUNDER_SFX_PHANTOM_GANON_GROUND_THUNDER:
+            return NA_SE_EN_FANTOM_THUNDER_GND;
+        case CONCURRENT_WEATHER_THUNDER_SFX_GANONDORF_THUNDER_IMPACT:
+            return NA_SE_EN_GANON_HIT_THUNDER;
+        case CONCURRENT_WEATHER_THUNDER_SFX_BARINADE_LIGHTNING_ATTACK:
+            return NA_SE_EN_BALINADE_THUNDER;
+        case CONCURRENT_WEATHER_THUNDER_SFX_OFF:
+        default:
+            return 0;
+    }
+}
 
 static inline void ConcurrentWeatherAudio_TrackNatureChannel(ConcurrentWeatherAudioState* state,
                                                               uint8_t channelRange, uint8_t port, uint8_t value,
