37da4cd6dd6f974a70bf7a55df9482b31ba92c9c
marsh6487 <marshcorey@gmail.com>
2026-09-03 09:57:18 -0500

    weather: harden active nature-channel guards (concurrent_weather_audio_test.c)

--- soh/tests/concurrent_weather_audio_test.c
+++ soh/tests/concurrent_weather_audio_test.c
@@ -6,21 +6,25 @@
 int main(void) {
     ConcurrentWeatherAudioState state = { 0 };
 
-    assert(ConcurrentWeatherAudio_ShouldPlayRainSfx(&state));
+    assert(ConcurrentWeatherAudio_ShouldPlayRainSfx(&state, 0x0028, 0x0001));
 
-    ConcurrentWeatherAudio_TrackNatureChannel(&state, 0x0E, 1, 1, 0x0E, 1);
-    assert(!ConcurrentWeatherAudio_ShouldPlayRainSfx(&state));
+    ConcurrentWeatherAudio_TrackNatureChannel(&state, 0x0E, 1, 1, 0x0E, 0x0F, 1);
+    assert(!ConcurrentWeatherAudio_ShouldPlayRainSfx(&state, 0x0001, 0x0001));
+    assert(ConcurrentWeatherAudio_ShouldPlayRainSfx(&state, 0x0028, 0x0001));
 
-    ConcurrentWeatherAudio_TrackNatureChannel(&state, 0x0F, 1, 1, 0x0E, 1);
-    assert(!ConcurrentWeatherAudio_ShouldPlayRainSfx(&state));
+    ConcurrentWeatherAudio_TrackNatureChannel(&state, 0x0F, 1, 1, 0x0E, 0x0F, 1);
+    assert(!ConcurrentWeatherAudio_ShouldPlayRainSfx(&state, 0x0001, 0x0001));
 
-    ConcurrentWeatherAudio_TrackNatureChannel(&state, 0x0E, 1, 0, 0x0E, 1);
-    assert(ConcurrentWeatherAudio_ShouldPlayRainSfx(&state));
+    ConcurrentWeatherAudio_TrackNatureChannel(&state, 0x0E, 1, 0, 0x0E, 0x0F, 1);
+    assert(ConcurrentWeatherAudio_ShouldPlayRainSfx(&state, 0x0001, 0x0001));
 
-    ConcurrentWeatherAudio_TrackNatureChannel(&state, 0x0E, 4, 1, 0x0E, 1);
-    assert(ConcurrentWeatherAudio_ShouldPlayRainSfx(&state));
+    ConcurrentWeatherAudio_TrackNatureChannel(&state, 0x0E, 4, 1, 0x0E, 0x0F, 1);
+    assert(ConcurrentWeatherAudio_ShouldPlayRainSfx(&state, 0x0001, 0x0001));
 
-    assert(!ConcurrentWeatherAudio_ShouldPlayThunder(0x0001, 0x0001));
-    assert(ConcurrentWeatherAudio_ShouldPlayThunder(0x0028, 0x0001));
+    ConcurrentWeatherAudio_TrackNatureChannel(&state, 0x0F, 1, 1, 0x0E, 0x0F, 1);
+    assert(!ConcurrentWeatherAudio_ShouldPlayThunderSfx(&state, 0x0001, 0x0001));
+    assert(ConcurrentWeatherAudio_ShouldPlayThunderSfx(&state, 0x0028, 0x0001));
+    ConcurrentWeatherAudio_TrackNatureChannel(&state, 0x0F, 1, 0, 0x0E, 0x0F, 1);
+    assert(ConcurrentWeatherAudio_ShouldPlayThunderSfx(&state, 0x0001, 0x0001));
     return 0;
 }
