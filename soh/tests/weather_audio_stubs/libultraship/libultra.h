9672540234d506adeff3c50162b3e4ba206da953
marsh6487 <marshcorey@gmail.com>
2026-09-09 22:34:09 -0500

    merge: add finalized weather support

---
+++ soh/tests/weather_audio_stubs/libultraship/libultra.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef int8_t s8;
+typedef int32_t s32;
+typedef float f32;
